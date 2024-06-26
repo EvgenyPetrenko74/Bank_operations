e2447da4501e77a04e5d89e7f3fe791425fcb0af
Your Name <pietrienko7474@mail.ru>
2024-06-26 12:28:07 +0600

    Добавлена функция форматирования даты

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from src.masks import get_mask_account, get_mask_card_number
 
 
@@ -11,6 +13,13 @@ def mask_account_card(string: str) -> str:
         return mask_card
 
 
+def get_data(data: str) -> str:
+    """Функция, которая возвращает строку с датой в требуемом формате."""
+    d = datetime.strptime(data, format("%Y-%m-%dt%H:%M:%S.%f"))
+    return d.strftime("%d.%m.%Y")
+
+
 if __name__ == "__main__":
     print(mask_account_card("Visa platinum 1111222233334444"))
     print(mask_account_card("Счет 11112222333344445555"))
+    print(get_data("2018-07-11T02:26:18.671407"))
