bd3a88d52acaa4c64e9b098cfc589a70ef2bc906
Your Name <pietrienko7474@mail.ru>
2024-06-29 22:42:48 +0600

    Добавлена функция сортировки словарей по убыванию даты

---
+++ src/processing.py
@@ -0,0 +1,46 @@
+from typing import Any
+
+original_lists = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
+
+def filter_by_state(original_list: list[dict[str, Any]], state_id: str = "EXECUTED") -> Any:
+    """Функция для фильтрации операций по ключу."""
+    sorted_list = []
+    for key in original_list:
+        if key.get("state") == state_id:
+            sorted_list.append(key)
+    return sorted_list
+
+
+def sort_by_date(original_list: list[dict[str, Any]], reverse_list: bool = True) -> list[dict[str, Any]]:
+    """Функция сортирует список словарей по убыванию даты."""
+    sorted_list = sorted(original_list, key=lambda d: d["date"], reverse=reverse_list)
+    return sorted_list
+
+
+if __name__ == "__main__":
+    print(
+        filter_by_state(
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ]
+        )
+    )
+    print(
+        sort_by_date(
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ]
+        )
+    )
