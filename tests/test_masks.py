38762b1c9bfb7fa586d2c96c367e3378831f3e4a
My Name <roman.petrenko.111@gmail.com>
2024-07-17 18:00:15 +0300

    Завершено написание тестов

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,19 +1,28 @@
 from src.masks import get_mask_card_number, get_mask_account
 import pytest
-@pytest.mark.parametrize("card_number, masked_card_number", [
-    ("1111222233334444", "1111 22** **** 4444"),
-    ("", " Номер отсутствует"),
-    ("jgcj", " Длина номера не верная или введены посторонние символы"),
-    ("6576554433", " Длина номера не верная или введены посторонние символы"),
-])
+
+
+@pytest.mark.parametrize(
+    "card_number, masked_card_number",
+    [
+        ("1111222233334444", "1111 22** **** 4444"),
+        ("", " Номер отсутствует"),
+        ("jgcj", " Длина номера не верная или введены посторонние символы"),
+        ("6576554433", " Длина номера не верная или введены посторонние символы"),
+    ],
+)
 def test_get_mask_card_number(card_number, masked_card_number):
     assert get_mask_card_number(card_number) == masked_card_number
 
-@pytest.mark.parametrize("account_number, masked_account_number", [
-    ("11112222333344445555", "** 5555"),
-    ("", " Номер отсутствует"),
-    ("jgcj", " Длина номера не верная или введены посторонние символы"),
-    ("6576554433", " Длина номера не верная или введены посторонние символы"),
-])
+
+@pytest.mark.parametrize(
+    "account_number, masked_account_number",
+    [
+        ("11112222333344445555", "** 5555"),
+        ("", " Номер отсутствует"),
+        ("jgcj", " Длина номера не верная или введены посторонние символы"),
+        ("6576554433", " Длина номера не верная или введены посторонние символы"),
+    ],
+)
 def test_get_mask_account(account_number, masked_account_number):
     assert get_mask_account(account_number) == masked_account_number
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,14 +1,20 @@
 from src.widget import mask_account_card, get_data
 import pytest
-@pytest.mark.parametrize("input_string, output_string", [
-    ("Visa platinum 1111222233334444", "Visa platinum 1111 22** **** 4444"),
-    ("Maestro 1111222233334444", "Maestro 1111 22** **** 4444"),
-    ("", "Данные отсутствуют или введены неверно"),
-    ("jgcj", "Данные отсутствуют или введены неверно"),
-    ("Счет 11112222333344445555", "Счет ** 5555"),
-])
+
+
+@pytest.mark.parametrize(
+    "input_string, output_string",
+    [
+        ("Visa platinum 1111222233334444", "Visa platinum 1111 22** **** 4444"),
+        ("Maestro 1111222233334444", "Maestro 1111 22** **** 4444"),
+        ("", "Данные отсутствуют или введены неверно"),
+        ("jgcj", "Данные отсутствуют или введены неверно"),
+        ("Счет 11112222333344445555", "Счет ** 5555"),
+    ],
+)
 def test_mask_account_card(input_string, output_string):
     assert mask_account_card(input_string) == output_string
+
 
 def test_get_data(data_string):
     assert get_data(data_string) == "11.07.2018"
